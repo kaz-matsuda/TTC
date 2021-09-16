69fb7fec9f462025fab44e51198ca065c8ae370e
kaz-matsuda <75776683+kaz-matsuda@users.noreply.github.com>
2021-09-16 09:13:33 +0900

    Add files via upload

--- TTC.py
+++ TTC.py
@@ -1,5 +1,8 @@
 #  TTC.py
 # preferenceデータから有向グラフを作り, TTC を適用するプログラム
+# 出力されるデータのファイル名を'afterTTC.csv'としています
+# afterTTC.csv の各行には, 左から順に
+#「交換希望者番号」「交換前のスロット」「->」「交換後のスロット」「交換相手」が出力されます
 
 import csv
 import random
@@ -7,16 +10,16 @@ import pprint
 import networkx as nx
 #import matplotlib.pyplot as plot
 
-student = 100 # S券交換を希望している学生の人数
+number_of_applicant = 800 # S券交換希望者の人数
 
 
 with open('test.csv') as f:
     reader = csv.reader(f)
     l = [row for row in reader]
-#print(l)
+
 
         
-g = open('afterTTC.txt', 'w')
+
 ##########################################    
 
 # input preferenceGraph:DiGraph
@@ -52,22 +55,22 @@ def Pull_out_all_cycles(preferenceGraph):
         cycle, residual = Pull_out_cycle(residual)
     return all_cycles, residual
 
-# input student, l
+# input number_of_applicant, l
 # output all_cycles, all_cycles2, residual
-def TTC(student, l):
+def TTC(number_of_applicant, l):
     # preference データから有向グラフを作る
     preferenceGraph = nx.DiGraph()
-    # 頂点の追加(個数 = student)   
+    # 頂点の追加(個数 = number_of_applicant)   
     i = 1
-    while i <= student:
+    while i <= number_of_applicant:
         preferenceGraph.add_node(i)
         i = i + 1
     # 有向辺の追加
     # 「A の◯スロット = B の交換前のスロット」が成り立つとき, 有向辺 A -> B を追加
     i = 1
-    while i <= student:
+    while i <= number_of_applicant:
         j = 1
-        while j <= student:
+        while j <= number_of_applicant:
             if (i != j) and (l[i-1][int(l[j-1][0])] == '1'):
                 preferenceGraph.add_edge(i,j)
             j = j + 1
@@ -82,9 +85,9 @@ def TTC(student, l):
     for x in sum(sum(all_cycles, []), []):
         already_removed_nodes.add(x)
     i = 1
-    while i <= student:
+    while i <= number_of_applicant:
         j = 1
-        while j <= student:
+        while j <= number_of_applicant:
             if (i != j) and (l[i-1][int(l[j-1][0])] == '2')  and not({i, j} & already_removed_nodes):
                 preferenceGraph2.add_edge(i,j)
             j = j + 1
@@ -93,11 +96,93 @@ def TTC(student, l):
     all_cycles2, residual = Pull_out_all_cycles(preferenceGraph2)
     return all_cycles, all_cycles2, residual
 
-all_cycles, all_cycles2, residual = TTC(student, l)
-list(map(print, all_cycles))
+
+
+all_cycles, all_cycles2, residual = TTC(number_of_applicant, l)
+
+# afterTTC.cvsファイルの作成
+
+# 全てのサイクルの辺のリストを作成する
+allCycle = []
+i = 1
+while i <= len(all_cycles):
+    j = 1
+    while j <= len(all_cycles[i-1]):
+        allCycle.append(all_cycles[i-1][j-1])
+        j = j + 1
+    i = i + 1
+
+i = 1
+while i <= len(all_cycles2):
+    j = 1
+    while j <= len(all_cycles2[i-1]):
+        allCycle.append(all_cycles2[i-1][j-1])
+        j = j + 1
+    i = i + 1
+
+# サイクルの辺に含まれない node i に対して, [i, -1] を追加しておく
+for j in residual.nodes:
+    allCycle.append([j, -1])
+
+allCycle.sort()
+#print(allCycle)
+
+# afterTTC.csvファイルの作成
+
+g = open('afterTTC.csv', 'w')
+
+i = 1
+while i <= number_of_applicant:
+    g.write(str(i) + ',')
+    # applicant No.i の, 交換前のスロットを書き込む
+    # 日付:int(l[i-1][0]) が 1~18:9/21, 19~36:9/22, 37~54:9/24
+    if 1 <= int(l[i-1][0]) <= 18 :
+        g.write('9/21 ')
+    elif 19 <= int(l[i-1][0]) <= 36:
+        g.write('9/22 ')
+    else:
+        g.write('9/24 ')
+    # 時:int(l[i-1][0]) が (1~6 or 19~24 or 37~42):14, (7~12 or 25~30 or 43~48):15, (13~18 or 31~36 or 49~54):16
+    if (1 <= int(l[i-1][0]) <= 6) or (19 <= int(l[i-1][0]) <= 24) or (37 <= int(l[i-1][0]) <= 42):
+        g.write('14:')
+    elif (7 <= int(l[i-1][0]) <= 12) or (25 <= int(l[i-1][0]) <= 30) or (43 <= int(l[i-1][0]) <= 48):
+        g.write('15:')
+    else:
+        g.write('16:')
+    # 分:int(l[i-1][0]) が (((6で割った余り) + 5) を 6 で割った余り)×10
+    g.write(str((( (int(l[i-1][0])%6) +5)%6)*10) + ',' + '->' + ',')
+    
+    # applicant No.i の交換が成立した場合, 交換後のスロットを書き込む
+    if allCycle[i-1][1] != -1:
+        # 日付:int(l[(allCycle[i-1][1])-1][0]) が 1~18:9/21, 19~36:9/22, 37~54:9/24
+        if 1 <= int(l[(allCycle[i-1][1])-1][0]) <= 18 :
+            g.write('9/21 ')
+        elif 19 <= int(l[(allCycle[i-1][1])-1][0]) <= 36:
+            g.write('9/22 ')
+        else:
+            g.write('9/24 ')
+        # 時:int(l[(allCycle[i-1][1])-1][0]) が (1~6 or 19~24 or 37~42):14, (7~12 or 25~30 or 43~48):15, (13~18 or 31~36 or 49~54):16
+        if (1 <= int(l[(allCycle[i-1][1])-1][0]) <= 6) or (19 <= int(l[(allCycle[i-1][1])-1][0]) <= 24) or (37 <= int(l[(allCycle[i-1][1])-1][0]) <= 42):
+            g.write('14:')
+        elif (7 <= int(l[(allCycle[i-1][1])-1][0]) <= 12) or (25 <= int(l[(allCycle[i-1][1])-1][0]) <= 30) or (43 <= int(l[(allCycle[i-1][1])-1][0]) <= 48):
+            g.write('15:')
+        else:
+            g.write('16:')
+        # 分:int(l[(allCycle[i-1][1])-1][0]) が (((6で割った余り) + 5) を 6 で割った余り)×10
+        g.write(str((( (int(l[(allCycle[i-1][1])-1][0])%6) +5)%6)*10))
+        g.write(',' + str(allCycle[i-1][1]) + '\n')
+    else:
+        g.write('no match' + '\n')
+    i = i + 1
+
+
+
+#list(map(print, all_cycles))
+#print(residual.nodes)
+#list(map(print, all_cycles2))
 print(residual.nodes)
-list(map(print, all_cycles2))
-print(residual.nodes)
+
+     
 
 g.write('end')
 f.close()
