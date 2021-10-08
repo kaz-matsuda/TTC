3783cf5630fac40010f22d862e1745497fded057
kaz-matsuda <75776683+kaz-matsuda@users.noreply.github.com>
2021-10-08 16:32:08 +0900

    Add files via upload

--- TTC.py
+++ TTC.py
@@ -10,10 +10,10 @@ import pprint
 import networkx as nx
 #import matplotlib.pyplot as plot
 
-number_of_applicant = 800 # S券交換希望者の人数
+number_of_applicant = 30 # preference.csv に載っている人数
 
 
-with open('test.csv') as f:
+with open('preference.csv') as f:
     reader = csv.reader(f)
     l = [row for row in reader]
 
@@ -133,43 +133,55 @@ g = open('afterTTC.csv', 'w')
 
 i = 1
 while i <= number_of_applicant:
-    g.write(str(i) + ',')
+    g.write(l[i-1][57] + ',' + l[i-1][58] + ',' + l[i-1][59] + ',' + l[i-1][60] + ',' + l[i-1][61] + ',')
     # applicant No.i の, 交換前のスロットを書き込む
-    # 日付:int(l[i-1][0]) が 1~18:9/21, 19~36:9/22, 37~54:9/24
+    # 日付:int(l[i-1][0]) が 1~18:9/21, 19~37:9/22, 38~56:9/24
     if 1 <= int(l[i-1][0]) <= 18 :
         g.write('9/21 ')
-    elif 19 <= int(l[i-1][0]) <= 36:
+    elif 19 <= int(l[i-1][0]) <= 37:
         g.write('9/22 ')
     else:
         g.write('9/24 ')
-    # 時:int(l[i-1][0]) が (1~6 or 19~24 or 37~42):14, (7~12 or 25~30 or 43~48):15, (13~18 or 31~36 or 49~54):16
-    if (1 <= int(l[i-1][0]) <= 6) or (19 <= int(l[i-1][0]) <= 24) or (37 <= int(l[i-1][0]) <= 42):
+    # 時:int(l[i-1][0]) が (1~6 or 19~24 or 38~43):14, (7~12 or 25~30 or 44~49):15, (13~18 or 31~36 or 50~55):16, (37 or 56):17
+    if (1 <= int(l[i-1][0]) <= 6) or (19 <= int(l[i-1][0]) <= 24) or (38 <= int(l[i-1][0]) <= 43):
         g.write('14:')
-    elif (7 <= int(l[i-1][0]) <= 12) or (25 <= int(l[i-1][0]) <= 30) or (43 <= int(l[i-1][0]) <= 48):
+    elif (7 <= int(l[i-1][0]) <= 12) or (25 <= int(l[i-1][0]) <= 30) or (44 <= int(l[i-1][0]) <= 49):
         g.write('15:')
-    else:
+    elif (13 <= int(l[i-1][0]) <= 18) or (31 <= int(l[i-1][0]) <= 36) or (50 <= int(l[i-1][0]) <= 55):
         g.write('16:')
+    else:
+        g.write('17:')
     # 分:int(l[i-1][0]) が (((6で割った余り) + 5) を 6 で割った余り)×10
-    g.write(str((( (int(l[i-1][0])%6) +5)%6)*10) + ',' + '->' + ',')
+    # int(l[i-1][0]) が 1~37 : (((6で割った余り) + 5) を 6 で割った余り)
+    # int(l[i-1][0]) が 38~56 : ((6で割った余り) + 4) を 6 で割った余り
+    if (1 <= int(l[i-1][0]) <= 37):
+        g.write(str(( (int(l[i-1][0])%6) +5)%6) + str(0) + ',' + '->' + ',')
+    else:
+        g.write(str(( (int(l[i-1][0])%6) +4)%6) + str(0) + ',' + '->' + ',')
     
     # applicant No.i の交換が成立した場合, 交換後のスロットを書き込む
     if allCycle[i-1][1] != -1:
-        # 日付:int(l[(allCycle[i-1][1])-1][0]) が 1~18:9/21, 19~36:9/22, 37~54:9/24
+        # 日付:int(l[(allCycle[i-1][1])-1][0]) が 1~18:9/21, 19~37:9/22, 38~56:9/24
         if 1 <= int(l[(allCycle[i-1][1])-1][0]) <= 18 :
             g.write('9/21 ')
-        elif 19 <= int(l[(allCycle[i-1][1])-1][0]) <= 36:
+        elif 19 <= int(l[(allCycle[i-1][1])-1][0]) <= 37:
             g.write('9/22 ')
         else:
             g.write('9/24 ')
-        # 時:int(l[(allCycle[i-1][1])-1][0]) が (1~6 or 19~24 or 37~42):14, (7~12 or 25~30 or 43~48):15, (13~18 or 31~36 or 49~54):16
-        if (1 <= int(l[(allCycle[i-1][1])-1][0]) <= 6) or (19 <= int(l[(allCycle[i-1][1])-1][0]) <= 24) or (37 <= int(l[(allCycle[i-1][1])-1][0]) <= 42):
+        # 時:int(l[(allCycle[i-1][1])-1][0]) が (1~6 or 19~24 or 38~43):14, (7~12 or 25~30 or 44~49):15, (13~18 or 31~36 or 50~55):16, (37 or 56):17
+        if (1 <= int(l[(allCycle[i-1][1])-1][0]) <= 6) or (19 <= int(l[(allCycle[i-1][1])-1][0]) <= 24) or (38 <= int(l[(allCycle[i-1][1])-1][0]) <= 43):
             g.write('14:')
-        elif (7 <= int(l[(allCycle[i-1][1])-1][0]) <= 12) or (25 <= int(l[(allCycle[i-1][1])-1][0]) <= 30) or (43 <= int(l[(allCycle[i-1][1])-1][0]) <= 48):
+        elif (7 <= int(l[(allCycle[i-1][1])-1][0]) <= 12) or (25 <= int(l[(allCycle[i-1][1])-1][0]) <= 30) or (44 <= int(l[(allCycle[i-1][1])-1][0]) <= 49):
             g.write('15:')
-        else:
+        elif (13 <= int(l[(allCycle[i-1][1])-1][0]) <= 18) or (31 <= int(l[(allCycle[i-1][1])-1][0]) <= 36) or (50 <= int(l[(allCycle[i-1][1])-1][0]) <= 55):
             g.write('16:')
-        # 分:int(l[(allCycle[i-1][1])-1][0]) が (((6で割った余り) + 5) を 6 で割った余り)×10
-        g.write(str((( (int(l[(allCycle[i-1][1])-1][0])%6) +5)%6)*10))
+        else:
+            g.write('17:')
+        # 分:int(l[(allCycle[i-1][1])-1][0]) が (((6で割った余り) + 5) を 6 で割った余り)
+        if (1 <= int(l[(allCycle[i-1][1])-1][0]) <= 37):
+            g.write(str((( (int(l[(allCycle[i-1][1])-1][0])%6) +5)%6)) + str(0))
+        else:
+            g.write(str((( (int(l[(allCycle[i-1][1])-1][0])%6) +4)%6)) + str(0))
         g.write(',' + str(allCycle[i-1][1]) + '\n')
     else:
         g.write('no match' + '\n')
